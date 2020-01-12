ac520340231ed0772fbcfddc61b4d80d3c7a4c7c
piotrslowik <33666830+piotrslowik@users.noreply.github.com>
2020-01-12 16:12:32 +0100

    Login css into scss

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -55,13 +55,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
 .LoginPage {
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    &-inputs {
+        width: 500px;
+    }
 }
-.LoginPage-inputs {
-    width: 500px;
-}
+
 </style>
